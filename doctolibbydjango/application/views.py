fb1bbc6d9faf9792d63d9283ebbd41280a3ad202
manu <arscaldgral@gmail.com>
2023-10-17 18:10:49 +0200

    commit

--- doctolibbydjango/application/views.py
+++ doctolibbydjango/application/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate
+from django.shortcuts import redirect
 from authentification.models import Utilisateur, medecinPatient
+from application.models import Symptome, Form_General
 
 @login_required
 def accueil(request):
@@ -71,9 +73,97 @@ def associationMedecinPatient(request):
                    "medecins" : medecins,
                    "tableAssociationMedecinPatient" : tableAssociationMedecinPatient})
 
+@login_required
+def evaluation_symptomes(request):
+  
+    # Liste des noms des champs
+    noms_des_champs = [
+        "Irritabilité",
+        "Sentiments dépressifs",
+        "Bouche sèche ou gorge sèche",
+        "Actions ou gestes impulsifs",
+        "Grincement des dents",
+        "Difficulté à rester assis",
+        "Cauchemars",
+        "Diarrhée",
+        "Attaques verbales envers quelqu’un",
+        "Hauts et bas émotifs",
+        "Grande envie de pleurer"
+    ]
+
+    print("------------------------------------------------------ ")
+
+    if request.method == 'POST':
+     
+        symptome_instance = Symptome()
+
+        symptome_instance.irratibilite = request.POST.get("Irritabilité")
+        symptome_instance.sentiment_depressif = request.POST.get("Sentiments dépressifs")
+        symptome_instance.bouche_gorge_seche = request.POST.get("Bouche sèche ou gorge sèche")
+        symptome_instance.actions_gestes_impulsif = request.POST.get("Actions ou gestes impulsifs")
+        symptome_instance.grincement_dents = request.POST.get("Grincement des dents")
+        symptome_instance.difficulte_a_rester_assis = request.POST.get("Difficulté à rester assis")
+        symptome_instance.cauchemars = request.POST.get("Cauchemars")
+        symptome_instance.diarrhee = request.POST.get("Diarrhée")
+        symptome_instance.attaques_verbales_envers_qq1 = request.POST.get("Attaques verbales envers quelqu’un")
+        symptome_instance.haut_bas_emotifs = request.POST.get("Hauts et bas émotifs")
+        symptome_instance.grande_envie_pleurer = request.POST.get("Grande envie de pleurer")
+
+        symptome_instance.save()
+
+        return redirect('/form-general/')
+
+    context = {
+        'choix_evaluation': Symptome.choises_evaluation,
+        'noms_des_champs': noms_des_champs
+    }
+    return render(request, 'evaluation_symptomes.html', context)
 
 
+def form_general_view(request):
+    if request.method == "POST":
+        # instance = Form_General()
 
 
+        print(request.POST)
+        # form = FormGeneralForm(request.POST)
+        # if form.is_valid():
+        #     form.save()
+        #     # Add some action here if needed (e.g., redirect to a success page)
 
+        # instance.save()
+
+    # else:
+    #     form = FormGeneralForm()
+        return redirect('/prise_medoc_alimentation/')
+
+    context = {
+        'choix_evaluation': Symptome.choises_evaluation,
+    }
+
+    return render(request, 'form_general.html', context)
+
+
+def prise_medoc_alimentation_view(request):
+    if request.method == "POST":
+        # instance = Form_General()
+
+
+        print(request.POST)
+        # form = FormGeneralForm(request.POST)
+        # if form.is_valid():
+        #     form.save()
+        #     # Add some action here if needed (e.g., redirect to a success page)
+
+        # instance.save()
+
+    # else:
+    #     form = FormGeneralForm()
+        pass
+
+    context = {
+        'choix_evaluation': Symptome.choises_evaluation,
+    }
+
+    return render(request, 'prise_medoc_alimentation.html', context)
 
